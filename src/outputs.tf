cad6e423ca6e54a8acc06f18068db9c19a1ca23c
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:19:16 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,14 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "name" {
+  value       = module.this.id
+  description = "The name of the namespace"
+}
+
+output "id" {
+  value       = coalesce(one(aws_service_discovery_http_namespace.default[*].id), one(aws_service_discovery_private_dns_namespace.default[*].id), one(aws_service_discovery_public_dns_namespace.default[*].id))
+  description = "The ID of the namespace"
+}
+
+output "arn" {
+  value       = coalesce(one(aws_service_discovery_http_namespace.default[*].arn), one(aws_service_discovery_private_dns_namespace.default[*].arn), one(aws_service_discovery_public_dns_namespace.default[*].arn))
+  description = "The ARN of the namespace"
 }
