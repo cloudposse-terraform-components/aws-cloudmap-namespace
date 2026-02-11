34a69bd2f29fda56763629546841bf162262f698
Igor Rodionov <496956+goruha@users.noreply.github.com>
2026-02-12 01:55:45 +0300

    Add security group and egress rule resources

--- src/main.tf
+++ src/main.tf
@@ -20,3 +20,20 @@ resource "aws_service_discovery_http_namespace" "default" {
   name        = module.this.id
   description = var.description
 }
+
+resource "aws_security_group" "default" {
+  count       = local.enabled && var.create_security_group ? 1 : 0
+  name        = module.this.id
+  description = "Allow all outbound traffic to any IPv4 address"
+  vpc_id      = module.vpc.outputs.vpc_id
+}
+
+resource "aws_security_group_rule" "allow_all_egress" {
+  count             = local.enabled && var.create_security_group ? 1 : 0
+  security_group_id = one(aws_security_group.default[*].id)
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
